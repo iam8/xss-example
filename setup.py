d202644f7097170d53e2c136298d1eb2fc477ff3
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-11 12:49:19 -0800

    Added my header and module docstring

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from model import db, Message 
+# Ioana A Mititean
+# 1/11/22
+# UWPCE Course 3 - Internet Programming in Python
+# Lesson 08 - Web Security and Class-Based Views
+
+"""
+Database setup script for class message board app.
+"""
+
+from model import db, Message
 
 db.connect()
 db.create_tables([Message])
